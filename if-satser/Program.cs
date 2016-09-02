987f648c43729fc3c53c28f8e4309ae806a84c16
wilhelmar <wilhelmar@vgy.se>
2016-09-02 14:37:55 +0200

    Gjorde Övning 1 och  2 om IF-satser

--- if-satser/Program.cs
+++ if-satser/Program.cs
@@ -10,6 +10,71 @@ namespace if_satser
     {
         static void Main(string[] args)
         {
+
+
+            /*
+                        //Övning 1
+
+                        double a;
+                        a = 1.6;
+                        Console.Write("Ange mönsterdjup på däck: ");
+                        double patterndepth = double.Parse(Console.ReadLine());
+                        if (patterndepth < a)
+                        {
+                            Console.WriteLine("Olagligt däck!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Lagligt däck!");
+                        }
+                        */
+
+            //Övning 2
+            int a, b, c, d, e, f, g, h;
+
+            a = 0;
+            b = 12;
+            c = 13;
+            d = 18;
+            e = 19;
+            f = 25;
+            g = 26;
+            h = 99;
+
+            Console.Write("Hur gammal är du?: ");
+            int age = int.Parse(Console.ReadLine());
+
+
+            if (age >= a && age <= b) 
+
+            {
+                Console.WriteLine("Färg: Vit");
+            }
+
+            else if (age >= c && age <= d)
+            {
+                Console.WriteLine("Färg: Grön");
+            }
+
+            else if (age >= e && age <= f)
+            {
+                Console.WriteLine("Färg: Röd");
+            }
+
+            else if (age >= g && age <= h) 
+            {
+                Console.WriteLine("Färg: Blå");
+            } 
+
+            else 
+            {
+                Console.WriteLine("Oglitig ålder angiven!");
+            }
+            
+        
+      
+
+          
         }
     }
 }
